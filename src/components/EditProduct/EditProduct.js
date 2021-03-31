942ba171f86b4c097b16eda1d08c6c63e67bff59
swapno <swapno5896@yahoo.com>
2021-03-31 10:52:49 -0700

    delete function is ready

--- src/components/EditProduct/EditProduct.js
+++ src/components/EditProduct/EditProduct.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
@@ -6,7 +6,18 @@ import './EditProduct.css'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 // import Loader from "react-loader-spinner";
 const EditProduct = (props) => {
-    const {name,photoUrl,features,price} =props.product
+    const {name,photoUrl,features,price,_id} =props.product
+
+    const deleteProduct =(event)=>{
+   console.log(event.target.value);
+          fetch(`http://localhost:5000/delet/${_id}`,{
+            method:'DElete'
+          })
+          .then(res=>res.json())
+        .then(data=>
+        console.log(data)
+      )
+    }
     return (
         <Grid item xs={12} sm={12} md={12} >
         <Paper style={{height:'100px'}} className='container'>
@@ -19,7 +30,7 @@ const EditProduct = (props) => {
 
   <div className="btn">
   <button className='button'>edit</button>
-    <button  className='button'>delet</button>
+    <button onClick={deleteProduct} className='button' value={_id}>delet</button>
   </div>
         </Paper>
       </Grid>
