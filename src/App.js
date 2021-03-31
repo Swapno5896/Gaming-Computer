5cd1737c5fc3f0533164d1c47952524397bab78f
swapno <swapno5896@yahoo.com>
2021-03-31 00:18:53 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import LogIn from './components/LogIn/LogIn'
+import CheckOut from './components/CheckOut/CheckOut'
+import Admin from "./components/Admin/Admin";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/logIn">Log in</Link>
+          </li>
+          <li>
+            <Link to="/checkOut">checkOut</Link>
+          </li>
+          <li>
+           <Link to="/admin">Admin</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path='/logIn'>
+         <LogIn></LogIn>
+        </Route>
+        <Route path='/checkOut'>
+          <CheckOut/>
+        </Route>
+        <Route path='/admin'>
+          <Admin></Admin>
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/components/AddProductd/AddProductd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddProductd = () => {
+    return (
+        <div>
+            <h3>this is add product</h3>
+        </div>
+    );
+};
+
+export default AddProductd;
