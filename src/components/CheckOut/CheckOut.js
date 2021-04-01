8d139d709377ca3ebbda7d0e21cfadcdf5f48dd3
swapno <swapno5896@yahoo.com>
2021-04-01 02:34:59 -0700

    check out is done

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import { useContext } from 'react';
+import { UserContext } from '../../App';
 const CheckOut = () => {
+    const [loggedInUser, setLoggedInUser,addedProduct, setAddedProduct] = useContext(UserContext);
+    const handelCheckOut = ()=>{
+        const order = {...loggedInUser,...addedProduct}
+        // fetch('http://localhost:5000/addToCart',{
+        //     method:'POST',
+        //     headers:{'Content-Type':'application/json'},
+        //         body:JSON.stringify(order)
+        //         .then(res=>res.JSON())
+        //         .then(data=>console.log(data))
+        //             })
+
+
+                    fetch('http://localhost:5000/addToCart',{
+                        method:'POST',
+                        headers:{'Content-Type':'application/json'},
+                        body:JSON.stringify([order])
+                    
+                      })
+
+    }
     return (
-        <div>
-            <h3>this is CheckOut</h3>
+        <div style={{margin:'50px 35%', display:'flex'}}>
+            <img style={{width:'50px'}} src={addedProduct.photoUrl} alt=""/>
+           <p>{addedProduct.name}</p>
+           <p>{addedProduct.price}</p>
+           <button onClick={handelCheckOut}>Check Out</button>
         </div>
     );
 };
